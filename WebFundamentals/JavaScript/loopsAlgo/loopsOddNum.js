5f7753506ad6430a57efa5eb928224bcb22100c6
rabindra malakar <rmalakar2013@gmail.com>
2020-12-15 16:56:04 -0600

    added 2 new javascript files to JavaScript folder

---
+++ WebFundamentals/JavaScript/loopsAlgo/loopsOddNum.js
@@ -0,0 +1,5 @@
+for (let num=1; num<20; num++) {
+    if (num % 2 !==0){
+        console.log(num)
+    }
+}
---
+++ WebFundamentals/JavaScript/loopsAlgo/sumNumber.js
@@ -0,0 +1,5 @@
+let sum = 0;
+for (let num=1; num<=5; num++){
+    sum = sum+num;
+    console.log("Num: " + num + " sum: " + sum)
+}
