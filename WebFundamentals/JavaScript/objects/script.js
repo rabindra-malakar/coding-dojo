f0bb26270c06b2a66fa3b9118a3d08d8801b11b4
rabindra malakar <rmalakar2013@gmail.com>
2020-12-22 13:54:47 -0600

    added new project files

---
+++ WebFundamentals/JavaScript/objects/script.js
@@ -0,0 +1,12 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+// How would you print/log John's age?
+console.log(users[1].age)
+// How would you print/log the name of the first object?
+console.log(users[0].name)
+// How would you print/log the name and age of each user using a for loop?  Your output should look something like this
+for (let i=0; i<users.length; i++){
+    console.log(users.name + " - " + users.age);
+}
+let user = users;
+console.log(user)
